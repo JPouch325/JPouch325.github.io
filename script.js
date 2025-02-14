49e6465915bf8fc4dd5175c758fb042922104cbc
Xyness <cmfarfait@icloud.com>
2025-02-14 11:21:41 +0100

    update

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const navLinks = document.querySelector(".nav-links");
 
         burgerMenu.addEventListener("click", function () {
+            this.classList.toggle("open");
             navLinks.classList.toggle("open");
         });
     });
