1c3d516818d518eb1aa6028b952254a7c0216081
Xyness <cmfarfait@icloud.com>
2025-11-21 01:47:21 +0100

    update

--- photo.py
+++ photo.py
@@ -1,12 +1,12 @@
 from PIL import Image
 import os
 
-input_folder = "ressources/galerie/studio"
-output_folder = "ressources/galerie/studio/thumbs"
+input_folder = "ressources/galerie/fall2025"
+output_folder = "ressources/galerie/fall2025/thumbs"
 os.makedirs(output_folder, exist_ok=True)
 
 for file in os.listdir(input_folder):
     if file.lower().endswith((".jpg", ".jpeg", ".png")):
         img = Image.open(os.path.join(input_folder, file))
-        img.thumbnail((800, 800))  # max 800px
+        img.thumbnail((800, 800))
         img.save(os.path.join(output_folder, file), "JPEG", quality=70)
